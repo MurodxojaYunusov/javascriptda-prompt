032a0a078e3a0fde8944fe48fa184bb5197f88dd
MurodxojaYunusov <murodxojayunusov492@gmail.com>
2023-05-21 08:46:35 +0500

    adad

---
+++ script.js
@@ -0,0 +1,42 @@
+// alert("Diqqat");
+
+var ism;
+
+if(confirm("Saytga hush kelibsiz!")){
+    ism = prompt("Ismingizni kiriting");
+    alert("yaxshimisiz " + ism) 
+}
+
+var manzil;
+
+if(confirm("Saytga hush kelibsiz!")){
+    manzil = prompt("manzilizni kiriting");
+    alert("mexmonga  chaqiring meni " + manzil) 
+}
+
+var yosh;
+
+if(confirm("Saytga hush kelibsiz!")){
+    yoshingiz = prompt("Yoshingizni kiriting");
+    alert("qoyil " + yoshingiz) 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
